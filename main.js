058dc34383c48f474e6824c5f6dfd9328ff9b180
Evanflow89 <ivanbaragone@gmail.com>
2022-05-02 18:37:19 +0200

    Added innerHtml on divs

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
-Consegna:
+// Consegna:
 // Scrivi un programma che stampi in console i numeri da 1 a 100.
 // Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
+const items = document.querySelector(".items");
 
 for (let number = 1; number <= 100; number++) {
+    const element = document.createElement("div");
+    element.classList.add("item");
+    items.append(element);
     if ((number % 3 == 0) && (number % 5 == 0)) {
+        element.classList.add("item");
+        element.innerHTML += "fizzbuzz";
         console.log("FizzBuzz");
     } else if (number % 3 == 0) {
+        element.classList.add("item");
+        element.innerHTML += "Fizz";
         console.log("Fizz");
     } else if (number % 5 == 0) {
+        element.classList.add("item");
+        element.innerHTML += "Buzz";
         console.log("Buzz");
-    } else console.log(number);
+    } else {
+        element.classList.add("item");
+        element.innerHTML += number;
+        console.log(number);
+    }
 }
+
+
 
 
 
