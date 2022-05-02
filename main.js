9c8d0883041d4cf18cb785cce01fb9c9e622ac35
Evanflow89 <ivanbaragone@gmail.com>
2022-05-02 15:41:56 +0200

    Step 1 Complete

--- main.js
+++ main.js
@@ -5,14 +5,13 @@ Consegna:
 
 
 for (let number = 1; number <= 100; number++) {
-    console.log(number);
     if ((number % 3 == 0) && (number % 5 == 0)) {
         console.log("FizzBuzz");
     } else if (number % 3 == 0) {
         console.log("Fizz");
     } else if (number % 5 == 0) {
         console.log("Buzz");
-    }
+    } else console.log(number);
 }
 
 
